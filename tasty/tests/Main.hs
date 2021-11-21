9339145819a3168e61d1b73d87472c41bf00c28a
Joona Piirainen <joona.piirainen@gmail.com>
2021-11-21 11:56:09 +0200

    parser tests

--- tasty/tests/Main.hs
+++ tasty/tests/Main.hs
@@ -3,9 +3,10 @@ module Main where
 import           Test.Tasty
 
 import           Tests.Lex
+import           Tests.Parse
 
 allTests :: TestTree
-allTests = testGroup "Top" [lexTests]
+allTests = testGroup "Top" [lexTests, parseTests]
 
 main :: IO ()
 main = defaultMain allTests
